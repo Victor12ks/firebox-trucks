8158706fcfe657ddb4a4a8d5311568d8f681f541
Joao Victor Sousa <victor12ks@gmail.com>
2022-04-12 21:20:31 -0300

    adicionado os models caminhao e modelo

---
+++ FireboxTrucks.Web/Models/Caminhao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FireboxTrucks.Web.Models
+{
+    public class Caminhao
+    {
+        public Caminhao()
+        {
+            AnoFabricacao = DateTime.Now.Year;
+        }
+
+        [Key]
+        public int ID { get; set; }
+
+        public int ModeloID { get; set; }
+
+        [MaxLength(4)]
+        [Display(Name = "Ano de Fabricação")]
+        [Required(ErrorMessage = "O campo Ano de Fabricação deve ser informado")]
+        [DataType(DataType.Text, ErrorMessage = "Ano de Fabricação em formato inválido")]
+        public int AnoFabricacao { get; set; }
+
+        [MaxLength(4)]
+        [Display(Name = "Ano do Modelo")]
+        [Required(ErrorMessage = "O campo Ano do Modelo deve ser informado")]
+        [DataType(DataType.Text, ErrorMessage = "Ano do Modelo em formato inválido")]
+        public int AnoModelo { get; set; }
+
+        public Modelo Modelo { get; set; }
+    }
+}
---
+++ FireboxTrucks.Web/Models/Modelo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FireboxTrucks.Web.Models
+{
+    public class Modelo
+    {
+        public Modelo()
+        {
+
+        }
+        public Modelo(string nome, string descricao)
+        {
+            Nome = nome;
+            Descricao = descricao;
+        }
+
+        [Key]
+        public int ID { get; private set; }
+        [Required]
+        [Display(Name = "Modelo")]
+        public string Nome { get; private set; }
+        [Required]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; private set; }
+    }
+}
