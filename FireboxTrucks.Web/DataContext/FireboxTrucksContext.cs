5ad1cb0b0fc595740b865968a9c1a23b3fcc69c0
Joao Victor Sousa <victor12ks@gmail.com>
2022-04-14 08:26:54 -0300

    removido modlbuilder do context

---
+++ FireboxTrucks.Web/DataContext/FireboxTrucksContext.cs
@@ -0,0 +1,20 @@
+﻿using FireboxTrucks.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FireboxTrucks.Web.DataContext
+{
+    public class FireboxTrucksContext : DbContext
+    {
+        public FireboxTrucksContext(DbContextOptions<FireboxTrucksContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Caminhao> Caminhao { get; set; }
+        public DbSet<Modelo> Modelo { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Caminhao>();
+            //modelBuilder.Entity<Modelo>();
+        }
+    }
+}
