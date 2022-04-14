c6105df44795a54f1ca33341e52047cb1f4ad46a
Joao Victor Sousa <victor12ks@gmail.com>
2022-04-14 08:26:25 -0300

    adicionado migrations

---
+++ FireboxTrucks.Web/Migrations/20220413013210_initial.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FireboxTrucks.Web.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Modelo",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modelo", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Caminhao",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ModeloID = table.Column<int>(type: "int", nullable: false),
+                    AnoFabricacao = table.Column<int>(type: "int", maxLength: 4, nullable: false),
+                    AnoModelo = table.Column<int>(type: "int", maxLength: 4, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Caminhao", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Caminhao_Modelo_ModeloID",
+                        column: x => x.ModeloID,
+                        principalTable: "Modelo",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Caminhao_ModeloID",
+                table: "Caminhao",
+                column: "ModeloID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Caminhao");
+
+            migrationBuilder.DropTable(
+                name: "Modelo");
+        }
+    }
+}
